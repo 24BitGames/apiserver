54f7b4dbaa2cb37fca3db208c9b3cacc097d1a30
Claudio <claudio@24bitgames.com>
2019-01-14 14:50:40 +0200

    Configured bodyParser limits to prevent 413s on larger levels.

--- web.js
+++ web.js
@@ -42,8 +42,8 @@ var router = express.Router();
 router.use(prepareRequests);
 
 // Configuration
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '10mb', extended:true}));
+app.use(bodyParser.json({limit: '10mb'}));
 
 if ('production' == app.get('env')) {
     process.on("uncaughtException", function (exceptionmessage) {
